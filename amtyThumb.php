ff973bdd8597023b1ea7f74f078909321ce5f135
Amit Gupta <amitguptagwl@users.noreply.github.com>
2017-12-02 11:56:23 +0530

    Update amtyThumb.php

--- amtyThumb.php
+++ amtyThumb.php
@@ -5,7 +5,7 @@ Plugin URI: http://article-stack.com/
 Description: This plugin fetch first image from your post even if it is vedio.
 
 Author: Amit Gupta
-Version: 4.1.2
+Version: 4.2.0
 Author URI: http://article-stack.com/
 */
 
